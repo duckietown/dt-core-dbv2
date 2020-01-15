6fe219d929613f70c221a9ce42fb324ce1d4c545
Timothy Scott <scottti@student.ethz.ch>
2020-01-15 17:18:53 +0100

    Implemented line follower lane controller

--- packages/line_follower_lane_controller/src/line_follower_lane_controller.py
+++ packages/line_follower_lane_controller/src/line_follower_lane_controller.py
@@ -26,6 +26,7 @@ class LineFollowerLaneController(DTROS):
         self.parameters['~max_I'] = None
         self.parameters['~I_decay'] = None
         self.parameters['~update_hz'] = None
+        self.parameters['~drive_speed'] = None
         self.parameters['~outer_right_threshold'] = None
         self.parameters['~inner_right_threshold'] = None
         self.parameters['~inner_left_threshold'] = None
@@ -47,27 +48,43 @@ class LineFollowerLaneController(DTROS):
             "line_following_node/line_follower", LineFollowerStamped, self.line_sensor_callback, queue_size=1
         )
 
+        self.car_cmd_pub = self.publisher(
+            "~car_cmd", Twist2DStamped, queue_size=1
+        )
+
     def line_sensor_callback(self, msg):
         current_time = time.time()
         duration = current_time - self.last_iteration_time
         detections = self.threshold_measurement(msg)
-        if not detections.inner_right and not detections.outer_right:
-            # Too far left
-            current_state = -1
-        elif not detections.inner_right and detections.outer_right:
-            # Perfect
-            current_state = 0
-        elif detections.inner_right and detections.outer_right:
-            # Too far right
-            current_state = 1
-        elif detections.inner_right and not detections.outer_right:
-            # WAY too far right
-            current_state = 2
+
+        # This dict maps line detections to a number representing the current state
+        states = {
+            # (Black, Black): Too far left
+            (False, False): -1,
+            # (Black, White): Perfectly on the line
+            (False, True): 0,
+            # (White, White): Too far right
+            (True, True): 1,
+            # (White, Black): Way too far right (past the line)
+            (True, False): 2
+        }
+
+        current_state = states[(detections.inner_right, detections.outer_right)]
 
         self.integral = self.parameters['~I_decay'] * (self.integral + current_state * duration)
+        # Constrain the integral to within +/- max_I
         self.integral = min(self.parameters['~max_I'], max(-self.parameters['~max_I'], self.integral))
 
-        self.log("State: {}, Integral: {}, Duration: {}".format(current_state, self.integral, duration))
+        omega = self.parameters['~steering_gain'] * current_state + self.parameters['~k_I'] * self.integral
+
+        self.log("State: {}, Integral: {}, Omega: {}, Raw measurements: ({}, {})".format(
+            current_state, self.integral, omega, msg.inner_right, msg.outer_right))
+
+        car_cmd = Twist2DStamped()
+        car_cmd.header.stamp = rospy.get_rostime()
+        car_cmd.v = self.parameters['~drive_speed']
+        car_cmd.omega = omega
+        self.car_cmd_pub.publish(car_cmd)
 
         self.prevState = current_state
         self.last_iteration_time = current_time
